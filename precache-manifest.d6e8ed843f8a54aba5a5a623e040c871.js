78f990b0bbdace61103d808a5bde6b4689be005f
Nigel Russell <nigeruss@googlemail.com>
2020-10-29 13:58:49 +0000

    Updates

---
+++ precache-manifest.d6e8ed843f8a54aba5a5a623e040c871.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c7fc102aa446a7c5da58a91d1206a02",
+    "url": "/Chucknorrisapp/index.html"
+  },
+  {
+    "revision": "5925ac3fcc8cb5e809d2",
+    "url": "/Chucknorrisapp/static/css/main.21d3986c.chunk.css"
+  },
+  {
+    "revision": "7eee4c48181d16ea8a45",
+    "url": "/Chucknorrisapp/static/js/2.0e487e42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Chucknorrisapp/static/js/2.0e487e42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5925ac3fcc8cb5e809d2",
+    "url": "/Chucknorrisapp/static/js/main.bcd5c808.chunk.js"
+  },
+  {
+    "revision": "c6f94396cc39865a8362",
+    "url": "/Chucknorrisapp/static/js/runtime-main.e8d0298b.js"
+  },
+  {
+    "revision": "15d220723ae19ab9f9de057b20b65e0b",
+    "url": "/Chucknorrisapp/static/media/cnpic.15d22072.png"
+  },
+  {
+    "revision": "03a61498fdceb889a05322cc7142e4dd",
+    "url": "/Chucknorrisapp/static/media/headliner.03a61498.ttf"
+  }
+]);
